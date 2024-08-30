7a5ba219282fd50322d520729fab3733b1b509f5
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-30 12:04:52 +0700

    Update commit
    Add: metadata opengraph image and update light/dark favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,25 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { m } from "framer-motion";
 
 const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: [
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      url: "/favicon-light.svg",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      url: "/favicon-dark.svg",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
   title: "Positivus",
   description: "a beautiful and functional landing page design created specifically for digital marketing agencies. With its clean and modern design, Positivus is the perfect template to showcase your agency's services and case studies to potential clients. Positivus comes complete with two screens - one for desktop and one for mobile - as well as a variety of styles and components, all designed using Figma's auto layout feature for maximum efficiency.",
 };
@@ -16,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${space_grotesk.className || ''} min-w-[1024px] max-w-screen-2xl  leading-normal tracking-normal antialiased px-[100px] overscroll-none`}>{children}</body>
+      <body className={`${space_grotesk.className || ''} sm:w-fit lg:w-full max-w-screen-2xl  leading-normal tracking-normal antialiased px-[20px] sm:px-[100px] overscroll-none`}>{children}</body>
     </html>
   );
 }
