1993ef191ba52a46f147407d5a0ebe2e3c6ff915
Phurin Nararat <phurin.nrt@gmail.com>
2024-08-31 23:35:30 +0700

    Update TestimonalBubble.tsx

--- src/app/ui/Testimonials/TestimonalBubble.tsx
+++ src/app/ui/Testimonials/TestimonalBubble.tsx
@@ -3,7 +3,7 @@ export default function TestimonalBubble({className=""}:{className?:string}) {
         <div className={`${className} flex flex-col gap-5`}>
             <div className="relative w-[330px] sm:w-[606px] h-full rounded-[45px] border border-green">
                 <div className="absolute w-16 h-16 rotate-45 border-b border-r border-green bg-dark -bottom-8 left-14"></div>
-                <p className="p-[30px] sm:p-[50px] rounded-[45px] bg-dark">"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."</p>
+                <p className="p-[30px] sm:p-[50px] rounded-[45px] bg-dark">&ldquo;We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence.&rdquo;</p>
             </div>
             <div className="mt-[45.25px] ml-[80px]">
                 <h4 className="text-green">John Smith</h4>
